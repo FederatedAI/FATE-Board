d8d033659f08785b800e0bdac6986994ab960733
v_wbymxu(许一鸣) <v_wbymxu@webank.com>
2020-02-05 17:00:39 +0800

    support client plugin
    Signed-off-by: v_wbymxu(许一鸣) <v_wbymxu@webank.com>

--- src/main/java/com/webank/ai/fate/board/conf/WebConfigration.java
+++ src/main/java/com/webank/ai/fate/board/conf/WebConfigration.java
@@ -48,18 +48,18 @@ public class WebConfigration implements WebMvcConfigurer {
         registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:8028").allowCredentials(true);
     }
 
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
-        converters.add(fastJsonHttpMessageConverter);
-    }
-
-    @Bean
-    public HttpMessageConverters fastJsonHttpMessageConverter() {
-        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
-        return new HttpMessageConverters(fastJsonHttpMessageConverter);
-
-    }
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+//        converters.add(fastJsonHttpMessageConverter);
+//    }
+//
+//    @Bean
+//    public HttpMessageConverters fastJsonHttpMessageConverter() {
+//        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+//        return new HttpMessageConverters(fastJsonHttpMessageConverter);
+//
+//    }
 
     @Bean("fateboardDataSource")
     @ConfigurationProperties(prefix = "fateboard.datasource")
