89d7d8c98f3e3047da4e096208fd4946ab51cf74
v_wbymxu <g6xuyiming@163.com>
2020-07-16 19:21:45 +0800

    fix security problem
    Signed-off-by: v_wbymxu <g6xuyiming@163.com>

--- src/main/java/com/webank/ai/fate/board/utils/LogHandle.java
+++ src/main/java/com/webank/ai/fate/board/utils/LogHandle.java
@@ -16,17 +16,16 @@ public class LogHandle {
         String boardPath = jarF.getParentFile().toString();
         int fateboard = boardPath.lastIndexOf("fateboard");
         String fatePath = boardPath.substring(0, fateboard);
-        String tmpPath = "/tmp/";
-        //            String ipRule = "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}";
-        String ipRule = "(127\\.0)|(localhost)|(10\\.\\d{1,3})|(172\\.((1[6-9])|(2\\d)|(3[01])))|(192\\.168)";
 
         for (Map stringObjectMap : result) {
             Object o = stringObjectMap.get(Dict.LOG_CONTENT);
             String log = (String) o;
             String relativePath = log.replaceAll(fatePath, "./fate/");
-            String relativePath2 = relativePath.replaceAll(tmpPath, "./");
 
-            String finalLog = relativePath2.replaceAll(ipRule, "xxx.xxx");
+//            String ipRule = "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}";
+            String ipRule="(127\\.0)|(localhost)|(10\\.\\d{1,3})|(172\\.((1[6-9])|(2\\d)|(3[01])))|(192\\.168)";
+
+            String finalLog = relativePath.replaceAll(ipRule, "xxx.xxx");
             stringObjectMap.put(Dict.LOG_CONTENT, finalLog);
 
         }
