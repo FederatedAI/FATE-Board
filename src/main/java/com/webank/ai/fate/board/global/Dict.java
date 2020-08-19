99f5dcf4448a48baf87d7c5e40ea3f9cef2fe537
v_wbymxu <g6xuyiming@163.com>
2020-08-19 19:10:04 +0800

    fix log problems
    Signed-off-by: v_wbymxu <g6xuyiming@163.com>

--- src/main/java/com/webank/ai/fate/board/global/Dict.java
+++ src/main/java/com/webank/ai/fate/board/global/Dict.java
@@ -63,8 +63,7 @@ public class Dict {
     static public final String URL_JOB_STOP = "/v1/job/stop";
     static public final String REMOTE_RETURN_MSG = "retmsg";
     static public final String URL_JOB_UPDATE = "/v1/job/update";
-    //todo need to update
-    static public final String URL_JOB_RESTART = "/v1/job/restart";
+    static public final String URL_JOB_RERUN = "/job/rerun";
 
     static public final String SSH_IP = "ip";
     static public final String SSH_USER = "user";
