d631aa6fb648545fca9d5c2cf3bc37d2997cef45
pizhicheng <pizhicheng@foxmail.com>
2022-07-26 16:31:58 +0800

    1.enable to config log level with url regex 2.enable to config more than one flow 3.update http client log format 4.remove redundant dependency and file
    Signed-off-by: zhichengpi <zhichengpi@webank.com>

--- src/main/java/com/webank/ai/fate/board/services/FlowFeign.java
+++ src/main/java/com/webank/ai/fate/board/services/FlowFeign.java
@@ -1,12 +1,13 @@
 package com.webank.ai.fate.board.services;
 
+import com.webank.ai.fate.board.conf.RouteTargeter;
 import com.webank.ai.fate.board.intercept.FeignRequestInterceptor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(url = "${fateflow.url}", name = "flowFeign", configuration = FeignRequestInterceptor.class)
+@FeignClient(url = RouteTargeter.URL_PLACE_HOLDER, name = "flowFeign", configuration = FeignRequestInterceptor.class)
 public interface FlowFeign {
 
     @RequestMapping(value = "{url}", method = RequestMethod.POST)
