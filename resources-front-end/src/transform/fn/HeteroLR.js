e444b4edd6f3f9e1f137a20e27942182a29139fb
ArvinHuang <1095369685@qq.com>
2022-11-25 16:57:06 +0800

    Mask useless model requests of NN and other components
    Signed-off-by: ArvinHuang <1095369685@qq.com>

--- resources-front-end/src/transform/fn/HeteroLR.js
+++ resources-front-end/src/transform/fn/HeteroLR.js
@@ -54,7 +54,7 @@ const createAsyncOption = (name, props, method, transform, exportName, detail) =
 })
 
 async function HeteroLRHandler(modelData, metricsData, partyId, role, componentName, jobId) {
-  const responseData = modelData.data.data
+  const responseData = modelData.data?.data
   const group = []
   let modelComponent
 
