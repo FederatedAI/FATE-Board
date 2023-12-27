0d2a1c4e6406b25aed5ba0ec286b1878ee75d006
ArvinHuang <1095369685@qq.com>
2023-12-27 11:24:40 +0800

    update: 1. Hint for canceling. 2. log display
    Signed-off-by: ArvinHuang <1095369685@qq.com>

--- resources-front-end/packages/fate-board/src/main.ts
+++ resources-front-end/packages/fate-board/src/main.ts
@@ -7,7 +7,6 @@ import 'fate-ui-component/dist/css/main.css';
 import * as FBVisualization from 'fate-visualization';
 import 'fate-visualization/dist/css/main.css';
 import * as Vue from 'vue';
-// import VueVirtualScroller from 'vue-virtual-scroller';
 import directive from './directive';
 import Router from './router/router';
 import Store from './store/store';
@@ -19,7 +18,6 @@ directive(app)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, <any>component)
 }
-// app.use(VueVirtualScroller)
 app.use(Store);
 app.use(Router);
 app.use(ElementPlus);
